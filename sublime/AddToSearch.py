1df95ceaed349a6620b3ff14323ac29cb9600743
turuslan <turuslan.devbox@gmail.com>
2022-09-02 10:12:29 +0300

    add multiple from search

--- sublime/AddToSearch.py
+++ sublime/AddToSearch.py
@@ -7,13 +7,14 @@ RE_FILE = re.compile("^([^ \t].*):$")
 RE_LINE = re.compile("^ +([0-9]+):")
 
 EXT = ".search"
+SYNTAX = "Packages/Default/Find Results.hidden-tmLanguage"
 def is_ext(path: typing.Optional[str]):
   return path.endswith(EXT) if path else False
 def is_ext_view(view: sublime.View):
   return is_ext(view.file_name())
 def set_syntax(view: sublime.View):
   # https://forum.sublimetext.com/t/find-results-files-a-way-to-open-it-after-it-was-saved/16094
-  view.assign_syntax("Packages/Default/Find Results.hidden-tmLanguage")
+  view.assign_syntax(SYNTAX)
   settings = view.settings()
   settings.set("detect_indentation", False)
   settings.set("line_numbers", False)
@@ -23,6 +24,11 @@ def set_syntax(view: sublime.View):
   settings.set("result_line_regex", RE_LINE.pattern)
   settings.set("scroll_past_end", True)
   settings.set("translate_tabs_to_spaces", False)
+
+T = typing.TypeVar("T")
+
+def filter2(xs: typing.List[T], f):
+  return ([x for x in xs if not f(x)], [x for x in xs if f(x)])
 
 class Settings:
   PROJECT = "AddToSearch"
@@ -66,13 +72,38 @@ class AddToSearchOpenCommand(sublime_plugin.TextCommand):
     settings = Settings.cmd(self)
     return bool(settings.add_to)
 
+def get_lines(view: sublime.View):
+  full = sublime.Region(0, view.size())
+  text = view.substr(full)
+  rlines = list(view.lines(full))
+  return full, rlines, [text[r.a:r.b] for r in rlines]
+
 Arg = typing.Tuple[str, typing.List[typing.Tuple[int, str]]]
 def make_arg(view: sublime.View) -> Arg:
   path = view.file_name()
   assert isinstance(path, str)
-  rs = sorted(sublime.Region(*r) for r in set(r.to_tuple() for s in view.sel() for r in view.lines(s)))
+  rs = sorted(sublime.Region(*r) for r in {r.to_tuple() for s in view.sel() for r in view.lines(s)})
   lines = [(view.rowcol(r.a)[0], view.substr(r)) for r in rs]
   return (path, lines)
+
+def make_arg2(view: sublime.View) -> typing.List[Arg]:
+  selected_lines = {view.rowcol(x)[0] for x in {r.a for s in view.sel() for r in view.lines(s)}}
+  _, _, lines = get_lines(view)
+  _args: typing.Dict[str, typing.Dict[str, typing.Tuple[int, str]]] = dict()
+  for path, g in g_files(lines):
+    _arg = _args.get(path, None)
+    def arg():
+      nonlocal _arg
+      if _arg is None:
+        _arg = dict()
+        _args[path] = _arg
+      return _arg
+    if g[0] in selected_lines:
+      arg()
+    for i, (j, s) in g[1]:
+      if i in selected_lines:
+        arg()[ukey(j, s)] = [j, s[1:] if s.startswith(" ") else s]
+  return [(path, sorted(_arg.values(), key=lambda x: x[0])) for path, _arg in _args.items()]
 
 def ukey(i: int, s: str):
   return "%d:%s" % (i, s.strip())
@@ -86,12 +117,14 @@ def g_lines(lines: typing.List[str], i: int):
     i += 1
 _Group = typing.Tuple[int, typing.Tuple[int, str]]
 Group = typing.Tuple[int, typing.List[_Group]]
+def _g_file(line: str):
+  m = RE_FILE.match(line)
+  return m and str(m[1])
 def g_files(lines: typing.List[str]):
   for i, line in enumerate(lines):
-    m = RE_FILE.match(line)
+    m = _g_file(line)
     if m:
-      s: str = m[1]
-      yield (s, (i, list(g_lines(lines, i + 1))))
+      yield (m, (i, list(g_lines(lines, i + 1))))
 
 def g_merge(lines: typing.List[str], arg: Arg):
   arg[1] = sorted(arg[1], key=lambda x: x[0])
@@ -122,14 +155,26 @@ def g_merge(lines: typing.List[str], arg: Arg):
       yield (i + 1, " %4d: %s" % (j + 1, s))
       mm = m + 1
 
+def g_merge2(lines: typing.List[str], args: typing.List[Arg]):
+  _args: typing.Dict[str, Arg] = dict()
+  for arg in args:
+    _arg = _args.get(arg[0], None)
+    if _arg is None:
+      _args[arg[0]] = arg
+    else:
+      _arg[1].extend(arg[1])
+  args = list(_args.values())
+  existing = set(filter(None, map(_g_file, lines)))
+  args = [x for xs in reversed(filter2(args, lambda x: x[0] in existing)) for x in xs]
+  for arg in args:
+    yield from g_merge(lines, arg)
+
 LOADING = "AddToSearch"
 class AddToSearchAddToCommand(sublime_plugin.TextCommand):
-  def run(self, edit: sublime.Edit, arg: Arg):
+  def run(self, edit: sublime.Edit, args: typing.List[Arg]):
     eol = "\n"
-    full = sublime.Region(0, self.view.size())
-    text = self.view.substr(full)
-    rlines = list(self.view.lines(full))
-    insert = sorted(g_merge([text[r.a:r.b] for r in rlines], arg), key=lambda x: x[0])
+    full, rlines, tlines = get_lines(self.view)
+    insert = sorted(g_merge2(tlines, args), key=lambda x: x[0])
     toffset = 0
     selection = self.view.sel()
     selection.clear()
@@ -144,15 +189,20 @@ class AddToSearchAddToCommand(sublime_plugin.TextCommand):
       toffset += len(tline) + len(eol)
 
 class AddToSearchAddCommand(sublime_plugin.TextCommand, WithPath):
+  @property
+  def search(self):
+    return is_ext(self.path) or self.view.settings().get("syntax") == SYNTAX
   def run(self, edit: sublime.Edit):
     settings = Settings.cmd(self)
     view = settings.window.open_file(settings.add_to)
+    args = make_arg2(self.view) if self.search else [make_arg(self.view)]
+    if not args: return
     if view.is_loading():
-      view.settings().set(LOADING, make_arg(self.view))
+      view.settings().set(LOADING, args)
     else:
-      view.run_command("add_to_search_add_to", dict(arg=make_arg(self.view)))
+      view.run_command("add_to_search_add_to", dict(args=args))
   def is_enabled(self):
-    if not self.path: return False
+    if not self.search and not self.path: return False
     settings = Settings.cmd(self)
     if self.path == settings.add_to: return False
     return bool(settings.add_to)
@@ -166,10 +216,10 @@ class ViewEvents(sublime_plugin.EventListener):
   def on_load(self, view: sublime.View):
     if is_ext_view(view):
       set_syntax(view)
-    arg = view.settings().get(LOADING)
-    if arg:
+    args = view.settings().get(LOADING)
+    if args:
       del view.settings()[LOADING]
-      view.run_command("add_to_search_add_to", dict(arg=arg))
+      view.run_command("add_to_search_add_to", dict(args=args))
 
   def on_post_save(self, view: sublime.View):
     if is_ext_view(view):
--- vscode/AddToSearch.ts
+++ vscode/AddToSearch.ts
@@ -28,6 +28,14 @@ function* zip<T, U>(ls: Iterable<T>, rs: Iterable<U>): Generator<[T, U]> {
   }
 }
 
+function filter2<T>(xs: Iterable<T>, f: (x: T) => boolean) {
+  const r: [T[], T[]] = [[], []];
+  for (const x of xs) {
+    r[f(x) ? 1 : 0].push(x);
+  }
+  return r;
+}
+
 function getLines(document: vscode.TextDocument) {
   const lines: vscode.TextLine[] = [];
   for (let i = 0; i < document.lineCount; ++i) {
@@ -54,21 +62,48 @@ async function withAddTo(
   await cb?.(editor);
 }
 
+function getSelectedLines(editor: vscode.TextEditor): Set<number> {
+  const selected_lines = new Set<number>();
+  for (const selection of editor.selections) {
+    for (let line = selection.start.line; line <= selection.end.line; ++line) {
+      selected_lines.add(line);
+    }
+  }
+  return selected_lines;
+}
+
 type Arg = [string, [number, string][]];
 function make_arg(editor: vscode.TextEditor): Arg {
   const { document } = editor;
-  const ilines = new Set<number>();
-  for (const sel of editor.selections) {
-    for (let line = sel.start.line; line <= sel.end.line; ++line) {
-      ilines.add(line);
-    }
-  }
   return [
     document.fileName,
-    Array.from(ilines).sort(cmp).map((iline) => document.lineAt(iline)).map(
-      (line) => [line.lineNumber, line.text],
-    ),
+    Array.from(getSelectedLines(editor)).sort(cmp).map((iline) => [
+      iline,
+      document.lineAt(iline).text,
+    ]),
   ];
+}
+
+function make_arg2(editor: vscode.TextEditor): Arg[] {
+  const selected_lines = getSelectedLines(editor);
+  const lines = getLines(editor.document).map((x) => x.text);
+  const args = new Map<string, Map<string, [number, string]>>();
+  for (const [path, g] of g_files(lines)) {
+    let _arg = args.get(path);
+    const arg = () => (_arg ?? args.set(path, _arg = new Map()), _arg);
+    if (selected_lines.has(g[0])) {
+      arg();
+    }
+    for (const [i, [j, s]] of g[1]) {
+      if (selected_lines.has(i)) {
+        arg().set(ukey(j, s), [j, s.startsWith(" ") ? s.slice(1) : s]);
+      }
+    }
+  }
+  return Array.from(args).map(([path, lines]) => [
+    path,
+    Array.from(lines.values()).sort(cmpf((x) => x[0])),
+  ]);
 }
 
 function ukey(i: number, s: string) {
@@ -84,6 +119,10 @@ function* g_lines(lines: string[], i: number): Generator<_Group> {
     ++i;
   }
 }
+function _g_file(line: string) {
+  const m = line.match(RE_FILE);
+  return m && m[1];
+}
 type _Group = [number, [number, string]];
 type Group = [number, _Group[]];
 function* g_files(
@@ -91,9 +130,9 @@ function* g_files(
 ): Generator<[string, Group]> {
   let i = 0;
   for (const line of lines) {
-    const m = line.match(RE_FILE);
+    const m = _g_file(line);
     if (m) {
-      yield [m[1], [i, Array.from(g_lines(lines, i + 1))]];
+      yield [m, [i, Array.from(g_lines(lines, i + 1))]];
     }
     ++i;
   }
@@ -138,6 +177,24 @@ function* g_merge(lines: string[], arg: Arg): Generator<[number, string]> {
   }
 }
 
+function* g_merge2(lines: string[], args: Arg[]): Generator<[number, string]> {
+  const _args = new Map<string, Arg>();
+  for (const arg of args) {
+    const _arg = _args.get(arg[0]);
+    if (_arg === undefined) {
+      _args.set(arg[0], arg);
+    } else {
+      _arg[1].push(...arg[1]);
+    }
+  }
+  args = Array.from(_args.values());
+  const existing = new Set(lines.map(_g_file).filter((x) => x));
+  args = filter2(args, (x) => existing.has(x[0])).reverse().flat();
+  for (const arg of args) {
+    yield* g_merge(lines, arg);
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerTextEditorCommand(
@@ -162,13 +219,18 @@ export function activate(context: vscode.ExtensionContext) {
       "AddToSearch.add",
       async (editor) => {
         const { document } = editor;
-        if (!document.fileName) {
+        const search = document.fileName.endsWith(EXT) ||
+          document.languageId === "search-result";
+        if (!search && !document.fileName) {
           return;
         }
         if (document.fileName === context.workspaceState.get(ADD_TO)) {
           return;
         }
-        const arg = make_arg(editor);
+        const args = search ? make_arg2(editor) : [make_arg(editor)];
+        if (args.length === 0) {
+          return;
+        }
         await withAddTo(context, async (editor) => {
           const { document } = editor;
           const lines = getLines(document);
@@ -177,7 +239,7 @@ export function activate(context: vscode.ExtensionContext) {
           if (last.isEmpty) {
             tlines.pop();
           }
-          const insert = Array.from(g_merge(tlines, arg))
+          const insert = Array.from(g_merge2(tlines, args))
             .sort(cmpf((x) => x[0]));
           const ilines: number[] = [];
           await editor.edit((edit) => {
